3ec32429fcf4436055c92dd43976f7370661a0d9
Yuudai Tamura <y.tamu63@gmail.com>
2024-10-14 11:45:05 +0900

    index.html,style.css,App.jsxの追加

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+export default function App() {
+    const showStory = false;
+    const xItem = "";
+    const yItem = "";
+    const zItem = "";
+    const ukus = "us";
+    return (
+      <>
+        <div>
+          <label htmlFor="customname">Enter custom name:</label>
+          <input type="text" placeholder="" />
+        </div>
+        <div>
+          <label htmlFor="us">US</label>
+          <input type="radio" value="us" checked={ukus === "us"} />
+          <label htmlFor="uk">UK</label>
+          <input type="radio" value="uk" checked={ukus === "uk"} />
+        </div>
+        <div>
+          <button>Generate random story</button>
+        </div>
+        {showStory && (
+          <p>
+            It was 94 fahrenheit outside, so {xItem} went for a walk. When they
+            got to {yItem}, they stared in horror for a few moments, then {zItem}.
+            Bob saw the whole thing, but was not surprised — {xItem} weighs 300
+            pounds, and it was a hot day.
+          </p>
+        )}
+      </>
+    );
+  }
